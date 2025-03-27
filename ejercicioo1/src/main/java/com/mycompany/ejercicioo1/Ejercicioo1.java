0503c0428e295721b04392a6d89eae798ed6d0a5
exe48319609 <exegonz203@gmail.com>
2025-03-27 16:34:41 -0300

    Add files via upload

---
+++ ejercicioo1/src/main/java/com/mycompany/ejercicioo1/Ejercicioo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicioo1;
+
+/**
+ *
+ * @author LABO B
+ */
+public class Ejercicioo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
